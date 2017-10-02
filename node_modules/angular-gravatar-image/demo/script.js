a8bf2ccb1962cb8fb4f5349529a01baf58b6e93e
Qli87 <luka.kulusic87@gmail.com>
2017-10-02 08:21:32 +0200

    code optimization

--- node_modules/angular-gravatar-image/demo/script.js
+++
@@ -1,9 +0,0 @@
-(function(angular) {
-  'use strict';
-
-  angular.module('gravatarImageExample', ['bgn.GravatarImage'])
-    .controller('GravatarImageController', ['$scope', function($scope) {
-      $scope.email = 'deerawan@gmail.com';
-    }]);
-
-})(window.angular);
